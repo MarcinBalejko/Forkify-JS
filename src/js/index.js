30a594712c284bb1278e0eaf18e3c9e15ea011e7
MarcinBalejko <mbbm9@o2.pl>
2020-05-04 22:11:20 +0200

    :white_check_mark: Recipe model complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from "./models/Search";
+import Recipe from "./models/Recipe";
 import * as searchView from "./views/searchView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
@@ -9,6 +10,8 @@ import { elements, renderLoader, clearLoader } from "./views/base";
  * - Liked recipes
  */
 const state = {};
+
+// SEARCH CONTROLLER
 
 const controlSearch = async () => {
   // 1.) Get query from the view
@@ -46,3 +49,9 @@ elements.searchResPages.addEventListener("click", (e) => {
     searchView.renderResults(state.search.result, goToPage);
   }
 });
+
+// RECIPE CONTROLLER
+
+const r = new Recipe(47746);
+r.getRecipe();
+console.log(r);
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -10,8 +10,26 @@ export default class Recipe {
       const res = await axios(
         `https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
       );
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.publisher;
+      this.img = res.data.recipe.image_url;
+      this.url = res.data.recipe.source_url;
+      this.ingredients = res.data.recipe.ingredients;
     } catch (error) {
       console.log(error);
+      alert("Something went wrong :(");
     }
+  }
+
+  calcTime() {
+    // 'ingredients' is an array of ingredients.
+    // Assuming that we need 15 min for each 3 ingredients:
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings() {
+    this.servings = 4;
   }
 }
