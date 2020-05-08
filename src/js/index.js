ecd3b34d4ac91a397970e0a309c24b3cb2e7f134
MarcinBalejko <mbbm9@o2.pl>
2020-05-08 23:51:28 +0200

    :white_check_mark: Restoring likes from localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -142,9 +142,6 @@ elements.shopping.addEventListener("click", (e) => {
  * LIKE CONTROLLER
  */
 
-state.likes = new Likes();
-//JUST FOR NOW ^^^
-
 const controlLike = () => {
   if (!state.likes) state.likes = new Likes();
   const currentID = state.recipe.id;
@@ -177,6 +174,20 @@ const controlLike = () => {
   }
   likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore liked recipes on page load
+window.addEventListener("load", () => {
+  state.likes = new Likes();
+
+  // Restore likes
+  state.likes.readStorage();
+
+  // Toggle like menu button
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+  // Render the existing likes
+  state.likes.likes.forEach((like) => likesView.renderLike(like));
+});
 
 // Handling recipe button clicks
 elements.recipe.addEventListener("click", (e) => {
